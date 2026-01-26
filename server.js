584c50eafceecfbde09c6ed61072d2c6682d1c24
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-26 14:27:01 -0500

    feat: sending a response using GET

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ import express from "express";
 
 const PORT = 8000;
 
+const celebrity = {
+  type: "action hero",
+  name: "JSON Statham",
+};
+
 const app = express();
+
+app.get("/", (req, res) => {
+  res.json(celebrity);
+});
 
 app.listen(PORT, () => `Server listening on port ${8000}`);
