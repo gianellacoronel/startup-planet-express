96d8648ea870a4423a212adea12d627920e1d187
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-26 14:33:04 -0500

    feat: getting data from startups

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 import express from "express";
+import { startups } from "./data/data.js";
 
-const PORT = 8000;
-
-const celebrity = {
-  type: "action hero",
-  name: "JSON Statham",
-};
+const PORT = 1234;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json(celebrity);
+app.get("/api", (req, res) => {
+  res.json(startups);
 });
 
 app.listen(PORT, () => `Server listening on port ${8000}`);
