bc7bec6287e27508918a45dc989d31f2c6200eba
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-26 15:44:23 -0500

    feat: adding route not found message

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 import express from "express";
 import { apiRouter } from "./routes/apiRoutes.js";
 
-const PORT = 8000;
-
 /*
 Challenge:
-1. Refactor the code to use express.Router()
+1. If a client uses an unknown route, serve this JSON
 
+{ message: "Endpoint not found. Please check the API documentation." }
+
+Remember to server an error code!
+
+Test:
+http://localhost:8000/wrong-api/useless/user
 */
+
+const PORT = 8000;
+
 const app = express();
 
 app.use("/api", apiRouter);
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Endpoint not found. Please check the API documentation.",
+  });
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${8000}`));
